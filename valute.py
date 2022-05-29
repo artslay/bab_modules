dadd900fbd0ff8fe1c062941832e42418037f7d7
artslay <artslay311@gmail.com>
2022-05-29 10:45:09 +0700

    update valute.py

--- valute.py
+++ valute.py
@@ -1,22 +1,20 @@
 import requests
-import datetime
 
-    
-    
 def cbr(event):
     dol = requests.get("https://www.cbr-xml-daily.ru/daily_json.js").json()
-    btc = requests.get("https://blockchain.info/ru/ticker").json()
-    
-    value_btc = btc["USD"]["last"]
+    btc = requests.get ("https://api.blockchain.com/v3/exchange/tickers").json()
+   
+
     value_usd = dol["Valute"]["USD"]["Value"]
     value_eur = dol ["Valute"]["EUR"]["Value"]
     name_eur= dol["Valute"]["EUR"]["Name"]
+    bt = btc[33]["last_trade_price"]
     
-    dol = f"""Курс валют:
-    Доллар : {round(value_usd)} ₽
+    doll = f"""Курс валют:
+    Доллар : {(value_usd)} ₽
     {name_eur} : {round(value_eur)} ₽
-    BTC : {value_btc} $
+    BTC : {bt} $
     """
-    event.message_send(f"{dol}")
+    event.message_send(doll)
     
 HandleCmd('курс', 0, cbr)
